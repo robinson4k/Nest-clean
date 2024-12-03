d3a530df3afc140960950885d2c167c74cce2f7c
Robinson Silva <robinson4k@gmail.com>
2024-12-03 08:14:28 -0400

    Controller de listagem de perguntas

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client"
 
-@Injectable()
+@Injectable() // decorator permite que este servico seja utilizado em outras partes da aplicacao
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     super({
